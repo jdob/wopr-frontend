a1ca0b9e8ba648f854c0b873bf03d298ebbce063
jdob <jdobies@redhat.com>
2019-10-28 14:32:23 -0400

    Minor tweaking to image

--- src/Pod.js
+++ src/Pod.js
@@ -6,7 +6,7 @@ class Pod extends Component {
     render() {
         return(
             <div className={"pod pod-status-" + this.props.phase.toLowerCase()}>
-                <img src="tic-tac-toe.gif" width="60px" height="60px" />
+                <img src="tic-tac-toe.gif" width="80px" height="80px" alt="TTT" />
                 <div className="pod-name">{this.props.name}</div>
                 <div className="pod-status">{this.props.phase}</div>
                 {/* <div className="pod-image"><strong>Image:</strong> {this.props.image}</div> */}
